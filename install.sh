757c57e84e3836eeab5f1fa4f09f0a33ed72649c
Pedro Cattori <pedro.cattori@tamr.com>
2016-09-12 11:56:24 -0400

    install wipes out old vim installation

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 # vim setup
 ###########
 
+# cleanup old setup
+rm ~/.vimrc && rm -rf ~/.vim
+mkdir -p ~/.vim/bundle
+
 # symlink to repo
 ln -s vimrc ~/.vimrc
+
 # get Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # get colorscheme
 git clone https://github.com/tomasr/molokai.git && \
   mv molokai/colors ~/.vim/colors && \
   rm -rf molokai
+
 # install plugins
 vim +PluginInstall +qall
