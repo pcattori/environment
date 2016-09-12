21367d2bb5ec83453b066bd46e82a65215b103d8
Pedro Cattori <pedro.cattori@tamr.com>
2016-09-12 11:51:45 -0400

    vim installation

---
+++ install.sh
@@ -0,0 +1,13 @@
+# vim setup
+###########
+
+# symlink to repo
+ln -s vimrc ~/.vimrc
+# get Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# get colorscheme
+git clone https://github.com/tomasr/molokai.git && \
+  mv molokai/colors ~/.vim/colors && \
+  rm -rf molokai
+# install plugins
+vim +PluginInstall +qall
