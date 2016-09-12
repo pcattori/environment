986c286dd0f3068880a113d466770a79b45b50d6
Pedro Cattori <pedro.cattori@tamr.com>
2016-09-12 15:59:25 -0400

    silence git clones

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ dotfiles=$(pwd)
 rm -rf ~/.vim && mkdir -p ~/.vim && mkdir -p ~/.vim/bundle
 
 # symlink to repo
-cd ~ && ln -sf ${dotfiles}/vimrc ~/.vimrc > /dev/null && cd -
+cd ~ && ln -sf ${dotfiles}/vimrc ~/.vimrc &> /dev/null && cd -
 
 # get Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim > /dev/null
 
 # get colorscheme
-git clone https://github.com/tomasr/molokai.git > /dev/null && \
+git clone https://github.com/tomasr/molokai.git &> /dev/null && \
   mv molokai/colors ~/.vim/colors && \
   rm -rf molokai
 
@@ -21,14 +21,14 @@ vim +PluginInstall +qall
 
 # git setup
 ###########
-cd ~ && ln -sf ${dotfiles}/gitconfig ~/.gitconfig > /dev/null && cd -
+cd ~ && ln -sf ${dotfiles}/gitconfig ~/.gitconfig &> /dev/null && cd -
 
 # bash setup
 ############
-cd ~ && ln -sf ${dotfiles}/bash_profile ~/.bash_profile > /dev/null && cd -
+cd ~ && ln -sf ${dotfiles}/bash_profile ~/.bash_profile &> /dev/null && cd -
 
-cd ~ && ln -sf ${dotfiles}/bashrc  ~/.bashrc > /dev/null && cd -
+cd ~ && ln -sf ${dotfiles}/bashrc  ~/.bashrc &> /dev/null && cd -
 
-cd ~ && ln -sf ${dotfiles}/alias ~/.alias > /dev/null && cd -
+cd ~ && ln -sf ${dotfiles}/alias ~/.alias &> /dev/null && cd -
 
 source ~/.bash_profile
