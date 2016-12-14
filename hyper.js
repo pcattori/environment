e6511728f0fd0f496ba3a38f5707817c4d9fc700
Pedro Cattori <pedro.cattori@tamr.com>
2016-12-14 15:34:34 +0100

    hyper term config

---
+++ hyper.js
@@ -0,0 +1,17 @@
+module.exports = {
+  config: {
+    fontSize: 24,
+    fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    cursorShape: 'BLOCK',
+    padding: '12px 14px',
+    bell: false,
+    // for advanced config flags please refer to https://hyper.is/#cfg
+  },
+  plugins: [
+    'hyperfull',
+    'hyper-blink',
+    'hyperterm-atom-dark', // theme
+    //'hyperterm-tabs' // https://github.com/zeit/hyper/issues/911
+    //'hyperterm-tab-icons' // not working with hyper v1.0.0
+  ]
+};
