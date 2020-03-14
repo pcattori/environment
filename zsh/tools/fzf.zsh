dddf9d276e6f1a569b113db433b7c19a29bfcdd7
Pedro Cattori <pcattori@gmail.com>
2020-03-14 12:07:56 -0400

    Add fzf as a dependency

--- zsh/tools/fzf.zsh
+++ zsh/tools/fzf.zsh
@@ -1,2 +1,16 @@
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+# adapted from .fzf.zsh
+
+FZF_HOME=~/.fzf
+
+# Generate fzf binary
+${FZF_HOME}/install --bin &> /dev/null
+
+export PATH=${FZF_HOME}/bin:$PATH
+
+# Auto-completion
+[[ $- == *i* ]] && source ${FZF_HOME}/shell/completion.zsh 2> /dev/null
+
+# Key bindings
+source ${FZF_HOME}/shell/key-bindings.zsh
+
 export FZF_DEFAULT_OPTS="--extended"
