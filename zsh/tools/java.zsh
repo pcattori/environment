05d30d852e5b08b1e220f37b2c1c921f2772f574
Pedro Cattori <pcattori@gmail.com>
2020-10-20 18:35:29 -0400

    remove work-specific tooling from personal branch

--- zsh/tools/java.zsh
+++
@@ -1,6 +0,0 @@
-# jabba
-[ -s "~/.jabba/jabba.sh" ] && source "~/.jabba/jabba.sh"
-
-# jenv
-export PATH="$HOME/.jenv/bin:$PATH"
-eval "$(jenv init -)"
--- zsh/tools/postgresapp.zsh
+++
@@ -1,5 +0,0 @@
-# latest
-# export PATH=/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH
-
-# 9.4
-export PATH=/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH
